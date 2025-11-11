b1d53d8643b1ba6bf2356f33e763993da9c7a1bb
andrewizzo <aizzo450@gmail.com>
2025-11-11 17:01:27 +0100

    sezione2

--- angular-signals/src/app/app.component.ts
+++ angular-signals/src/app/app.component.ts
@@ -1,12 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
 
+
+type Options = Record<string, string>
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-signals';
+  // title = 'angular-signals';
+
+  // -------------------------------------------------------------------
+  
+  // readonly counter$ = interval(1000);
+
+  // readonly changeDetector =inject(ChangeDetectorRef)
+
+  // doNothing(){}
+
+  // -------------------------------------------------------------------
+
+  readonly options$ = new BehaviorSubject<Options>({'r' : 'Red', 'g' : 'green', 'b' : 'blue'})
+
+  readonly selectedKey$ = new BehaviorSubject<string>('b');
+
+  readonly selectedValue$ = combineLatest([this.options$,this.selectedKey$]).pipe(
+    map(([options,key]) => options[key])
+  )
+
+  switchOptions(){
+    this.options$.next({'m' : 'magenta', 'y':'yellow', 'c':'cyan'})
+    this.selectedKey$.next('c');
+  }
+ 
+  constructor(){
+    // setInterval( () => {
+    //   this.counter++
+    //   console.log('Counter:' , this.counter);
+    // },1000);
+
+    // setInterval( () => {
+    //   this.changeDetector.detectChanges();
+    // },3000)
+
+    // -------------------------------------------------------------------
+    
+    this.selectedValue$.subscribe(console.log);
+  }
 }
